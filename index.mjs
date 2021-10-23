2592153b7d2d4a6b62193e5c358f34a872c56794
DarkEye <lesko.matej.pu@gmail.com>
2021-10-23 22:36:50 +0200

    lamp abstraction integration

--- index.mjs
+++ index.mjs
@@ -1,25 +1,23 @@
-import SerialPort from "serialport";
-import constants from "./constants.mjs";
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+import Lamp from './lamp.mjs';
+const app = express();
+const port = 3000;
 
-const sPort = new SerialPort("/dev/rfcomm0", {
-	baudRate: 9600,
+const lamps = {
+  bedroom: new Lamp('/dev/rfcomm0'),
+};
+
+app.get('/lamp/:position/toggleLight', (req, res) => {
+  lamps[req.params.position].toggleLight(err => {
+    if (err) {
+      res.send(err.message);
+      res.sendStatus(500);
+      return;
+    }
+    res.sendStatus(200);
+  });
 });
 
-app.get('/', (req, res) => {
-	sPort.write(constants.BT_TOGGLE_LIGHT, function (err) {
-		if (err) {
-			res.send(err.message)
-			res.sendStatus(500)
-			return console.log("Error -> Toggle Light:", err.message);
-		}
-		console.log("Toggle Light: success");
-		res.sendStatus(200);
-	});
-})
-
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
