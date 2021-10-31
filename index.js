10df66c7a4ccb8d62d2439ba6b9cc3de37a7d510
DarkEye <lesko.matej.pu@gmail.com>
2021-10-31 19:19:27 +0100

    cors usage

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 
-import fs from "fs";
-const dir = "./build";
+import fs from 'fs';
+const dir = './build';
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors({ origin: 'http://localhost:4000' }));
 
-if (fs.existsSync(dir)) {
-  console.info("Frontend 'build' directory exists!");
-  const FEApp = await import("./build/middlewares.js");
-  app.use(
-    FEApp.assetsMiddleware,
-    FEApp.prerenderedMiddleware,
-    FEApp.kitMiddleware
-  );
-} else {
-  console.warn("Frontend 'build' directory not found!");
-}
-
-import Lamp from "./lamp.mjs";
-import testLamp from "./mocks/lamp.mjs";
-import endpoints from "./endpoints.mjs";
+import Lamp from './lamp.mjs';
+import testLamp from './mocks/lamp.mjs';
+import endpoints from './endpoints.mjs';
 
 const lamps = {
-  bedroom: new Lamp("/dev/rfcomm1", "bedroom"),
-  proto: new Lamp("/dev/rfcomm0", "proto"),
+  bedroom: new Lamp('/dev/rfcomm1', 'bedroom'),
+  proto: new Lamp('/dev/rfcomm0', 'proto'),
   mock: testLamp,
 };
 
 function _executeCMD(res, lamp, cmd, ...parameters) {
   if (lamp && !lamp.errorState) {
-    lamp[cmd]((err) => {
+    lamp[cmd](err => {
       if (err) {
         res.status(500).send(err.message);
         return;
@@ -43,27 +33,27 @@ function _executeCMD(res, lamp, cmd, ...parameters) {
 
 app.get(endpoints.toggleLight, (req, res) => {
   const lamp = lamps[req.params.position];
-  _executeCMD(res, lamp, "toggleLight");
+  _executeCMD(res, lamp, 'toggleLight');
 });
 
 app.get(endpoints.enableMotionSensor, (req, res) => {
   const lamp = lamps[req.params.position];
-  _executeCMD(res, lamp, "turnOn", "motion");
+  _executeCMD(res, lamp, 'turnOn', 'motion');
 });
 
 app.get(endpoints.disableMotionSensor, (req, res) => {
   const lamp = lamps[req.params.position];
-  _executeCMD(res, lamp, "turnOff", "motion");
+  _executeCMD(res, lamp, 'turnOff', 'motion');
 });
 
 app.get(endpoints.enableSoundSensor, (req, res) => {
   const lamp = lamps[req.params.position];
-  _executeCMD(res, lamp, "turnOn", "sound");
+  _executeCMD(res, lamp, 'turnOn', 'sound');
 });
 
 app.get(endpoints.disableSoundSensor, (req, res) => {
   const lamp = lamps[req.params.position];
-  _executeCMD(res, lamp, "turnOff", "sound");
+  _executeCMD(res, lamp, 'turnOff', 'sound');
 });
 
 app.get(endpoints.synchronize, (req, res) => {
@@ -81,8 +71,23 @@ app.get(endpoints.synchronize, (req, res) => {
   }
 });
 
-app.listen(port, ["127.0.0.1", "192.168.0.239", "192.168.0.233"], () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-  console.log(`Example app listening at http://192.168.0.239:${port}`); // ntb
-  console.log(`Example app listening at http://192.168.0.233:${port}`); // PI
-});
+if (fs.existsSync(dir)) {
+  console.info("Frontend 'build' directory exists!");
+  const FEApp = await import('./build/middlewares.js');
+  app.use(
+    FEApp.assetsMiddleware,
+    FEApp.prerenderedMiddleware,
+    FEApp.kitMiddleware
+  );
+} else {
+  console.warn("Frontend 'build' directory not found!");
+}
+app.listen(
+  port,
+  ['localhost', '127.0.0.1', '192.168.0.239', '192.168.0.233'],
+  () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://192.168.0.239:${port}`); // ntb
+    console.log(`Example app listening at http://192.168.0.233:${port}`); // PI
+  }
+);
